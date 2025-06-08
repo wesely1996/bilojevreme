b56b4de145cbd67751b10873f06114e468aec259
wesely1996 <nikola.wesely.96@gmail.com>
2025-06-08 16:11:27 +0200

    Added child guest options

--- rsvp.js
+++ rsvp.js
@@ -5,6 +5,42 @@ document
     plusOneFields.style.display = this.checked ? "block" : "none";
   });
 
+document.getElementById("kidsToggle").addEventListener("change", function () {
+  const kidsNumberDiv = document.getElementById("kidsNumber");
+
+  const show = this.checked;
+  kidsNumberDiv.style.display = show ? "block" : "none";
+  document.getElementById("kidsNumberInput").value = "0";
+  const kidsLabel = document.getElementById("kidsLabel");
+  kidsLabel.style.display = "none";
+
+  // Hide all kid fields by default
+  for (let i = 1; i <= 3; i++) {
+    document.getElementById(`kid${i}`).style.display = "none";
+    if (!show) document.getElementById(`kid${i}Input`).value = "";
+  }
+});
+
+document
+  .getElementById("kidsNumberInput")
+  .addEventListener("focus", function () {
+    this.select();
+    setTimeout(() => this.select(), 0);
+  });
+
+document
+  .querySelector('input[name="entry.2142379312"]') // Number of kids
+  .addEventListener("input", function () {
+    const numKids = Math.min(3, parseInt(this.value) || 0);
+    const kidsLabel = document.getElementById("kidsLabel");
+    kidsLabel.style.display = numKids > 0 ? "block" : "none";
+    for (let i = 1; i <= 3; i++) {
+      const kid = document.getElementById(`kid${i}`);
+      kid.style.display = i <= numKids ? "block" : "none";
+      if (i > numKids) document.getElementById(`kid${i}Input`).value = "";
+    }
+  });
+
 const form = document.getElementById("rsvp-form");
 const thankYou = document.getElementById("thank-you");
 
@@ -19,6 +55,34 @@ form.addEventListener("submit", function (e) {
   if (!plusOneToggle.checked) {
     // Ensure empty string is added when +1 is not checked
     formData.set(plusOneInputName, "");
+  }
+
+  if (!document.getElementById("kidsToggle").checked) {
+    formData.set("entry.2142379312", "0");
+    formData.set("entry.1800873322", "");
+    formData.set("entry.867450465", "");
+    formData.set("entry.1055618791", "");
+  } else {
+    const numKids = Math.min(
+      3,
+      parseInt(document.getElementById("kidsNumberInput").value) || 0
+    );
+    switch (numKids) {
+      case 1:
+        formData.set("entry.867450465", "");
+        formData.set("entry.1055618791", "");
+        break;
+      case 2:
+        formData.set("entry.1055618791", "");
+        break;
+      case 3:
+        break;
+      default:
+        formData.set("entry.1800873322", "");
+        formData.set("entry.867450465", "");
+        formData.set("entry.1055618791", "");
+        break;
+    }
   }
 
   fetch(
